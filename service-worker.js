151acca9af43bd2007427b19c2df03c68cd08e31
Antonio Pelusi <antoniopelusi2000@gmail.com>
2026-02-16 17:36:30 +0100

    v1.1.1

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,61 @@
 const CACHE_NAME = "recoil-island";
 const urlsToCache = [
-    "",
-    "index.html",
-    "assets/css/main.css",
-    "assets/js/main.js",
-    "favicon.ico",
-    "assets/fonts/PixelifySans-Medium.ttf",
-    "assets/fx/action.wav",
-    "assets/fx/death.wav",
-    "assets/fx/shot.wav",
-    "assets/fx/startlevel.wav",
-    "assets/img/blood.png",
-    "assets/img/enemy.png",
-    "assets/img/grass1.png",
-    "assets/img/grass2.png",
-    "assets/img/obstacle1.png",
-    "assets/img/obstacle2.png",
-    "assets/img/obstacle3.png",
-    "assets/img/obstacle4.png",
-    "assets/img/obstacle5.png",
-    "assets/img/obstacle6.png",
-    "assets/img/obstacle7.png",
-    "assets/img/obstacle8.png",
-    "assets/img/obstacle9.png",
-    "assets/img/player.png",
-    "assets/img/sand.png",
-    "assets/img/water.png",
-    "assets/maps/map0.json",
-    "assets/maps/map1.json",
-    "assets/maps/map2.json",
-    "assets/maps/map3.json",
-    "assets/maps/map4.json",
-    "assets/maps/map5.json",
-    "assets/maps/map6.json",
-    "assets/maps/map7.json",
-    "assets/maps/map8.json",
-    "assets/maps/map9.json",
-    "assets/icons/apple-touch-icon.png",
-    "assets/icons/favicon.ico",
-    "assets/icons/favicon.svg",
-    "assets/icons/favicon-96x96.svg",
-    "assets/icons/site.webmanifest",
-    "assets/icons/web-app-manifest-192x192.png",
-    "assets/icons/web-app-manifest-512x512.png",
+    "./",
+    "./index.html",
+    "./assets/css/main.css",
+    "./assets/js/main.js",
+    "./favicon.ico",
+    "./assets/fonts/PixelifySans-Medium.ttf",
+    "./assets/fx/action.wav",
+    "./assets/fx/death.wav",
+    "./assets/fx/shot.wav",
+    "./assets/fx/startlevel.wav",
+    "./assets/img/blood.png",
+    "./assets/img/enemy.png",
+    "./assets/img/grass1.png",
+    "./assets/img/grass2.png",
+    "./assets/img/obstacle1.png",
+    "./assets/img/obstacle2.png",
+    "./assets/img/obstacle3.png",
+    "./assets/img/obstacle4.png",
+    "./assets/img/obstacle5.png",
+    "./assets/img/obstacle6.png",
+    "./assets/img/obstacle7.png",
+    "./assets/img/obstacle8.png",
+    "./assets/img/obstacle9.png",
+    "./assets/img/player.png",
+    "./assets/img/sand.png",
+    "./assets/img/water.png",
+    "./assets/maps/map0.json",
+    "./assets/maps/map1.json",
+    "./assets/maps/map2.json",
+    "./assets/maps/map3.json",
+    "./assets/maps/map4.json",
+    "./assets/maps/map5.json",
+    "./assets/maps/map6.json",
+    "./assets/maps/map7.json",
+    "./assets/maps/map8.json",
+    "./assets/maps/map9.json",
+    "./assets/icons/apple-touch-icon.png",
+    "./assets/icons/favicon.ico",
+    "./assets/icons/favicon.svg",
+    "./assets/icons/favicon-96x96.png",
+    "./assets/icons/site.webmanifest",
+    "./assets/icons/web-app-manifest-192x192.png",
+    "./assets/icons/web-app-manifest-512x512.png",
 ];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+        caches.open(CACHE_NAME).then((cache) => {
+            return Promise.all(
+                urlsToCache.map((url) => {
+                    return cache
+                        .add(url)
+                        .catch((err) => console.error("Missing asset:", url));
+                }),
+            );
+        }),
     );
     self.skipWaiting();
 });
@@ -69,10 +77,16 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
     if (event.request.method !== "GET") return;
+
     event.respondWith(
-        caches
-            .match(event.request)
-            .then((response) => response || fetch(event.request))
-            .catch(() => caches.match("index.html")),
+        caches.match(event.request).then((response) => {
+            if (response) return response;
+
+            return fetch(event.request).catch(() => {
+                if (event.request.mode === "navigate") {
+                    return caches.match("./") || caches.match("index.html");
+                }
+            });
+        }),
     );
 });
